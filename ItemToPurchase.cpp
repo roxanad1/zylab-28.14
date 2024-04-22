5696d7e1a91b819ce4eb4998b93273ee9a05a180
roxanad1 <167349781+roxanad1@users.noreply.github.com>
2024-04-22 13:20:19 -0500

    Update ItemToPurchase.cpp
    added code

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,31 @@
-/* Type your code here */
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase(){
+}
+
+void ItemToPurchase::SetName(string name) {
+  itemName = name;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+  itemPrice = price;
+}
+
+void ItemToPurchase::SetQuantity(int qty) {
+  itemQuantity = qty;
+}
+
+string ItemToPurchase::GetName(){
+  return itemName;
+}
+
+int ItemToPurchase::GetPrice(){
+  return itemPrice;
+}
+
+int ItemToPurchase::GetQuantity(){
+  return itemQuantity;
+}
